62aacffdde5e108ec66d9d2c8b7b8270e84a0516
Sean Soria <sean.soria@gmail.com>
2012-10-30 14:19:55 -0700

    update teleport platforms to behave more like portal

--- Assets/CODE/TeleportPlatform.cs
+++ Assets/CODE/TeleportPlatform.cs
@@ -11,7 +11,11 @@ public class TeleportPlatform : MonoBehaviour
             return;
         }
 
-        c.transform.position = Destination.transform.TransformPoint(transform.InverseTransformPoint(c.transform.position));
-        c.rigidbody.velocity = Destination.transform.TransformDirection(transform.InverseTransformDirection(c.rigidbody.velocity));
+        // Transform position and velocity to new platform
+        //c.transform.position = Destination.transform.TransformPoint(transform.InverseTransformPoint(c.transform.position));
+        //c.rigidbody.velocity = Destination.transform.TransformDirection(transform.InverseTransformDirection(c.rigidbody.velocity));
+
+        c.transform.position = Destination.transform.position;
+        c.rigidbody.velocity = c.rigidbody.velocity.magnitude * Destination.transform.up;
     }
 }
