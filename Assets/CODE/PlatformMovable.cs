c2321c5f280a66289b09b212ee6326283dfa8b9e
Sean Soria <sean.soria@gmail.com>
2012-10-30 16:57:29 -0700

    adds user movable platforms -scs

---
+++ Assets/CODE/PlatformMovable.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+
+public class PlatformMovable : MonoBehaviour
+{
+    bool moving;
+
+    void Update()
+    {
+        if (moving)
+        {
+            transform.position += getDeltaPosition();
+        }
+    }
+
+    void OnMouseDown()
+    {
+        moving = true;
+        getDeltaPosition();
+    }
+
+    void OnMouseUp()
+    {
+        moving = false;
+    }
+
+    Vector3 previousPosition;
+    Vector3 getDeltaPosition()
+    {
+        Vector3 currentPosition = getCurrentPosition();
+        Vector3 deltaPosition = currentPosition - previousPosition;
+        previousPosition = currentPosition;
+
+        return deltaPosition;
+    }
+
+    Vector3 getCurrentPosition()
+    {
+        Camera camera = Camera.main;
+        Vector3 mousePosition = Input.mousePosition;
+        return camera.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, camera.nearClipPlane));
+    }
+}
