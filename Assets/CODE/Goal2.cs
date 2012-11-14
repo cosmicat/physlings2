117d72eae3b49fb3e627db41b5bc05cf047f82f5
peters pc laptop <fish@robotbear.org>
2012-11-13 19:08:11 -0800

    adde new assets

---
+++ Assets/CODE/Goal2.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+
+public class Goal2 : MonoBehaviour {
+    float mTimer = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (mTimer <= 0)
+            renderer.enabled = true;
+        else
+        {
+            mTimer -= Time.deltaTime;
+            renderer.enabled = false;
+        }
+
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        mTimer = 2;
+    }
+}
